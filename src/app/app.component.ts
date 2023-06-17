0a6696cf56f4b79b8e7e24325346ddbe92fa5290
Hares-Thumar <hareshthumar46@gmail.com>
2023-06-17 16:46:41 +0530

    responsive mat-navbar with sidebar in angular 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-responsive-sidebar';
+  constructor() { }
+  sideBarMode = 'side';
+  toggleButton = false;
+  ngOnInit() {
+    this.windowSize();
+  }
+
+  getBrowserSize() {
+    var w: any, h: any;
+
+    if (typeof window.innerWidth != 'undefined') {
+      w = window.innerWidth; //other browsers
+      h = window.innerHeight;
+    }
+    else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+      w = document.documentElement.clientWidth; //IE
+      h = document.documentElement.clientHeight;
+    }
+    else {
+      w = document.body.clientWidth; //IE
+      h = document.body.clientHeight;
+    }
+    return { 'width': w, 'height': h };
+  }
+
+
+  windowSize() {
+    console.warn(this.getBrowserSize().width);
+    if (parseInt(this.getBrowserSize().width) < 980) {
+      this.sideBarMode = 'over';
+      this.toggleButton = true;
+    }
+    else {
+      this.sideBarMode = 'side';
+      this.toggleButton = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
